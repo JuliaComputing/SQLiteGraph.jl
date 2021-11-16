9d42beeff766ea2e70716befc45a786a1de44712
joshday <emailjoshday@gmail.com>
2021-11-15 20:19:43 -0500

    wip

--- README.md
+++ README.md
@@ -2,13 +2,73 @@
 
 A Graph Database for Julia, built on top of [SQLite.jl](https://github.com/JuliaDatabases/SQLite.jl).
 
+## Quickstart
+
+- *Nodes* must have "properties", even if `nothing`.
+- *Edges* must have "properties", even if `nothing`.
+
+
+### Creating a Graph Database
+
+```julia
+using SQLiteGraph
+
+db = DB()
+# SimpleGraphDB(":memory:") (0 nodes, 0 edges)
+```
+
+### Adding Nodes
+
+```julia
+db[1] = (x=1, y=2) 
+# properties must be `JSON3.write`-able (saved in the SQLite database as TEXT)
+db[2] = (x=1, y=10)
+
+db[1] 
+# "{\"x\":1,\"y\":2}"
+```
+
+### Adding Edges 
+
+```julia
+db[1,2] = (a=1, b=2)
+
+db[1,2]
+# "{\"a\":1,\"b\":2}"
+```
+
+### `ReadAs`
+
+- You can wrap a `DB` with `ReadAs` to enforce how you want the `TEXT` to be `JSON3.read`:
+
+```julia
+rdb = ReadAs(db, Dict{String, Int})
+# ReadAs{Dict{String, Int64}}: SimpleGraphDB(":memory:") (2 nodes, 1 edges)
+
+rdb[1]
+# Dict{String, Int64} with 2 entries:
+#   "x" => 1
+#   "y" => 2
+
+rdb[1,2]
+# Dict{String, Int64} with 2 entries:
+#   "b" => 2
+#   "a" => 1
+```
+
+
 
 ## Attribution
 
-SQLiteGraph is **Strongly** influenced (much has been copied verbatim) by [https://github.com/dpapathanasiou/simple-graph](https://github.com/dpapathanasiou/simple-graph).  
+SQLiteGraph is **STRONGLY** influenced (much has been copied verbatim) from [https://github.com/dpapathanasiou/simple-graph](https://github.com/dpapathanasiou/simple-graph).  
 
-The differences here are minor, opinionated changes made by `@joshday`.
+The differences here are minor, opinionated changes made by `@joshday`:
+
+- Node IDs are `Int`: 1, 2, 3...
+- Both `nodes` and `edges` tables have field `props`
 
 ## TODOs
 
 - Prepare SQL into compiled `SQLite.Stmt`s.
+- querying
+- traverse
--- src/SQLiteGraph.jl
+++ src/SQLiteGraph.jl
@@ -4,7 +4,7 @@ using SQLite: SQLite
 import DBInterface: execute
 using JSON3: JSON3
 
-export DB, Node, Edge
+export DB, ReadAs
 
 #-----------------------------------------------------------------------------# utils 
 read_sql(fn::String) = read(joinpath(@__DIR__, "sql", fn), String)
@@ -71,6 +71,7 @@ n_nodes(db::DB) = single_result_execute(db, "SELECT Count(*) FROM nodes")
 n_edges(db::DB) = single_result_execute(db, "SELECT Count(*) FROM edges")
 
 Base.length(db::DB) = n_nodes(db)
+Base.size(db::DB) = (n_nodes(db), n_edges(db))
 Base.lastindex(db::DB) = length(db)
 
 init!(db::DB, n) = (foreach(id -> setindex!(db, nothing, id), 1:n); db)
@@ -81,11 +82,12 @@ struct ReadAs{T}
     db::DB 
 end
 ReadAs(db::DB, T::DataType=Dict{String,Any}) = ReadAs{T}(db)
+Base.show(io::IO, r::ReadAs{T}) where {T} = (print(io, "ReadAs{$T}: "); print(io, r.db))
 Base.setindex!(r::ReadAs, args...) = setindex!(r.db, args...)
-Base.getindex(r::ReadAs{T}, args...) where {T} = (res=r[args...]; isnothing(res) ? res : JSON3.read(res, T))
+Base.getindex(r::ReadAs{T}, args...) where {T} = (res=r.db[args...]; isnothing(res) ? res : JSON3.read(res, T))
+Base.deleteat!(r::ReadAs, args...) = deleteat!(r.db, args...)
 
-
-#-----------------------------------------------------------------------------# set/get node(s)
+#-----------------------------------------------------------------------------# nodes
 function Base.setindex!(db::DB, props, id::Integer)
     execute(db, "INSERT INTO nodes VALUES(?, json(?))", (id, JSON3.write(props)))
     db
@@ -94,14 +96,23 @@ function Base.getindex(db::DB, id::Integer)
     res = single_result_execute(db, "SELECT props FROM nodes WHERE id = ?", (id,))
     isnothing(res) ? throw(BoundsError(db, id)) : res
 end
+function Base.deleteat!(db::DB, id::Integer)
+    execute(db, "DELETE FROM nodes WHERE id = ?", (id,))
+    execute(db, "DELETE FROM edges WHERE source = ? OR target = ?", (id, id))
+    db
+end
 
-#-----------------------------------------------------------------------------# set/get edge(s)
+#-----------------------------------------------------------------------------# edges
 function Base.setindex!(db::DB, props, i::Integer, j::Integer)
     execute(db, "INSERT INTO edges VALUES(?, ?, json(?))", (i, j, JSON3.write(props)))
     db
 end
 function Base.getindex(db::DB, i::Integer, j::Integer) 
     single_result_execute(db, "SELECT props FROM edges WHERE source = ? AND target = ? ", (i,j))
+end
+function Base.deleteat!(db::DB, i::Integer, j::Integer)
+    execute(db, "DELETE FROM edges WHERE source = ? AND target = ?", (i, j))
+    db
 end
 
 end
