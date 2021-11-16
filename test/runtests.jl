0de03173e3ea02241b57dafbc8ca6c4e1bb5290b
joshday <emailjoshday@gmail.com>
2021-11-16 10:39:30 -0500

    begin CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using SQLiteGraph
 using Test
 
 @testset "SQLiteGraph.jl" begin
-    # Write your tests here.
+    @testset "empty DB" begin 
+        db = DB()
+        @test length(db) == 0 
+        @test size(db) == (nodes=0, edges=0)
+        @test_throws BoundsError db[5]
+        @test isnothing(db[1,2])
+    end
 end
