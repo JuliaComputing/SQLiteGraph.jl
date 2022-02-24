468422d8e340cdeb9c87638762afe8415461b3ee
Josh Day <emailjoshday@gmail.com>
2022-02-24 10:17:20 -0500

    wip rewrite

--- src/SQLiteGraph.jl
+++ src/SQLiteGraph.jl
@@ -5,7 +5,7 @@ import DBInterface: execute
 using JSON3: JSON3
 using EasyConfig
 
-export DB, Node, Edge, find_nodes, find_edges
+export DB, Node, Edge
 
 #-----------------------------------------------------------------------------# utils
 function single_result_execute(db, stmt, args...)
@@ -30,102 +30,120 @@ end
 # Relationships always has a direction (one direction).
 # Relationships must have a type (one type) to define (classify) what type of relationship they are.
 
+# Nouns-nodes, Adjectives-properties, Verbs-relationship, Adverbs-properties on relationship
+
+# Property Graph Model on page 4
+# https://s3.amazonaws.com/artifacts.opencypher.org/openCypher9.pdf
+
 struct Node
     id::Int
     labels::Vector{String}
     props::Config
 end
 Node(id::Int, labels::String...; props...) = Node(id, collect(labels), Config(props))
+Node(row::SQLite.Row) = Node(row.id, split(row.labels, ';'), JSON3.read(row.props, Config))
 function Base.show(io::IO, o::Node)
-    print(io, "Node($(join(o.labels, ", "))) | $(o.id) | nprops=$(length(o.props))")
+    printstyled(io, "Node(", color=:light_cyan)
+    printstyled(io, join(repr.(o.labels), ", "), color=:light_yellow)
+    printstyled(io, ", ", o.id, color=:light_black)
+    printstyled(io, ")", color=:light_cyan)
+    if isempty(o.props)
+        printstyled(io, " with no props", color=:light_black)
+    else
+        printstyled(io, " with props: ", color=:light_black)
+        printstyled(io, join(keys(o.props), ", "), color=:light_green)
+    end
 end
+args(n::Node) = (n.id, join(n.labels, ';'), JSON3.write(n.props))
 
 
-struct Relationship
-    source_id::Int
-    target_id::Int
+struct Edge
+    source::Int
+    target::Int
     type::String
     props::Config
 end
-Relationship(src::Int, tgt::Int, type::String; props...) = Relationship(src, tgt, type, Config(props))
-function Base.show(io::IO, o::Relationship)
-    print(io, "Relationship($(o.type)) | $(o.source_id) → $(o.target_id) | nprops=$(length(o.props))")
+Edge(src::Int, tgt::Int, type::String; props...) = Edge(src, tgt, type, Config(props))
+Edge(row::SQLite.Row) = Edge(row.source, row.target, row.type, JSON3.read(row.props, Config))
+function Base.show(io::IO, o::Edge)
+    printstyled(io, "Edge(", color=:light_cyan)
+    printstyled(io, repr(o.type), color=:light_yellow)
+    printstyled(io, ", $(o.source) → $(o.target)", color=:light_black)
+    printstyled(io, ")", color=:light_cyan)
+    if isempty(o.props)
+        printstyled(io, " with no props", color=:light_black)
+    else
+        printstyled(io, " with props: ", color=:light_black)
+        printstyled(io, join(keys(o.props), ", "), color=:light_green)
+    end
+end
+args(e::Edge) = (e.source, e.target, join(e.type, ';'), JSON3.write(e.props))
+
+#-----------------------------------------------------------------------------# DB
+struct DB
+    sqlitedb::SQLite.DB
+
+    function DB(file::String = ":memory:")
+        db = SQLite.DB(file)
+        foreach(x -> execute(db, x), [
+            # nodes
+            "CREATE TABLE IF NOT EXISTS nodes (
+                id INTEGER NOT NULL UNIQUE PRIMARY KEY,
+                labels TEXT,
+                props TEXT,
+                UNIQUE(id) ON CONFLICT REPLACE
+            );",
+            "CREATE INDEX IF NOT EXISTS id_idx ON nodes(id);",
+            "CREATE INDEX IF NOT EXISTS labels_idx ON nodes(labels);",
+
+            # edges
+            "CREATE TABLE IF NOT EXISTS edges (
+                source INTEGER NOT NULL,
+                target INTEGER NOT NULL,
+                type TEXT NOT NULL,
+                props TEXT,
+                FOREIGN KEY(source) REFERENCES nodes(id),
+                FOREIGN KEY(target) REFERENCES nodes(id)
+            );",
+            "CREATE INDEX IF NOT EXISTS source_idx ON edges(source);",
+            "CREATE INDEX IF NOT EXISTS target_idx ON edges(target);",
+            "CREATE INDEX IF NOT EXISTS type_idx ON edges(type);",
+        ])
+        new(db)
+    end
+end
+function Base.show(io::IO, db::DB)
+    print(io, "SQLiteGraph.DB(\"$(db.sqlitedb.file)\") ($(n_nodes(db)) nodes, $(n_edges(db)) edges)")
+end
+
+execute(db::DB, args...; kw...) = execute(db.sqlitedb, args...; kw...)
+
+n_nodes(db::DB) = single_result_execute(db, "SELECT Count(*) FROM nodes")
+n_edges(db::DB) = single_result_execute(db, "SELECT Count(*) FROM edges")
+
+Base.length(db::DB) = n_nodes(db)
+Base.size(db::DB) = (n_nodes=n_nodes(db), n_edges=n_edges(db))
+
+#-----------------------------------------------------------------------------# nodes
+function Base.getindex(db::DB, id::Integer)
+    res = execute(db, "SELECT * FROM nodes WHERE id = ?", (id,))
+    isempty(res) ? throw(BoundsError(db, id)) : Node(first(res))
+end
+
+function Base.push!(db::DB, node::Node)
+    res = execute(db, "SELECT * FROM nodes WHERE id=?", (node.id,))
+    isempty(res) ?
+        execute(db, "INSERT INTO nodes VALUES(?, ?, json(?))", args(node)) :
+        error("Node with id=$(node.id) already exists in graph.  Use `insert!` to overwrite.")
+    db
+end
+function Base.insert!(db::DB, node::Node)
+    execute(db, "INSERT INTO nodes VALUES(?, ?, json(?)) ON CONFLICT(id) DO UPDATE SET labels=excluded.labels, props=excluded.props", args(node))
+    db
 end
 
 
 
-
-# #-----------------------------------------------------------------------------# Node
-# struct Node
-#     id::Int
-#     props::Config
-# end
-# Node(id::Integer; props...) = Node(id, Config(props))
-# Node(id::Integer, props::String) = Node(id, JSON3.read(props, Config))
-# function Base.show(io::IO, o::Node)
-#     props = getfield(o, :props)
-#     printstyled(io, "Node $(getfield(o, :id))", color=:light_cyan)
-#     delete_empty!(props)
-#     print_props(io, props)
-# end
-# Base.getproperty(o::Node, x::Symbol) = getfield(o, :props)[x]
-# Base.setproperty!(o::Node, x::Symbol, val) = setproperty!(getfield(o, :props), x, val)
-
-# #-----------------------------------------------------------------------------# Edge
-# struct Edge
-#     source::Int
-#     target::Int
-#     props::Config
-# end
-# Edge(src::Integer, tgt::Integer; kw...) = Edge(src, tgt, Config(kw))
-# Edge(src::Integer, tgt::Integer, txt::String) = Edge(src, tgt, JSON3.read(txt, Config))
-# function Base.show(io::IO, o::Edge)
-#     props = getfield(o, :props)
-#     printstyled(io, "Edge $(getfield(o, :source)) → $(getfield(o, :target))", color=:light_cyan)
-#     delete_empty!(props)
-#     print_props(io, props)
-# end
-# Base.getproperty(o::Edge, x::Symbol) = getfield(o, :props)[x]
-# Base.setproperty!(o::Edge, x::Symbol, val) = setproperty!(getfield(o, :props), x, val)
-
-
-# #-----------------------------------------------------------------------------# DB
-# struct DB
-#     sqlitedb::SQLite.DB
-
-#     function DB(file::String = ":memory:")
-#         db = SQLite.DB(file)
-#         foreach(x -> execute(db, x), [
-#             "CREATE TABLE IF NOT EXISTS nodes (
-#                 id INTEGER NOT NULL UNIQUE,
-#                 props TEXT,
-#                 UNIQUE(id) ON CONFLICT REPLACE
-#             );",
-#             "CREATE INDEX IF NOT EXISTS id_idx ON nodes(id);",
-#             "CREATE TABLE IF NOT EXISTS edges (
-#                 source INTEGER,
-#                 target INTEGER,
-#                 props  TEXT,
-#                 UNIQUE(source, target) ON CONFLICT REPLACE,
-#                 FOREIGN KEY(source) REFERENCES nodes(id),
-#                 FOREIGN KEY(target) REFERENCES nodes(id)
-#             );",
-#             "CREATE INDEX IF NOT EXISTS source_idx ON edges(source);",
-#             "CREATE INDEX IF NOT EXISTS target_idx ON edges(target);"
-#         ])
-#         new(db)
-#     end
-# end
-# function Base.show(io::IO, db::DB)
-#     print(io, "SQLiteGraph.DB(\"$(db.sqlitedb.file)\") ($(n_nodes(db)) nodes, $(n_edges(db)) edges)")
-# end
-
-# execute(db::DB, args...; kw...) = execute(db.sqlitedb, args...; kw...)
-
-# n_nodes(db::DB) = single_result_execute(db, "SELECT Count(*) FROM nodes")
-# n_edges(db::DB) = single_result_execute(db, "SELECT Count(*) FROM edges")
-
-# init!(db::DB, n::Integer) = (foreach(id -> setindex!(db, nothing, id), 1:n); db)
 
 # #-----------------------------------------------------------------------------# get/set nodes
 # function Base.setindex!(db::DB, props, id::Integer)
